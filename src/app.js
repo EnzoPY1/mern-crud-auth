04962f2bc5ecaee33edb31d90e9e14bf2d4b9889
Enzo Durañona <eduranona@bepsa.com.py>
2024-06-25 16:44:03 -0400

    segundo commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
+import taskRoutes from './routes/task.routes.js'
 
 const app = express();
 app.use(morgan('dev'));
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
 
 export default app;
